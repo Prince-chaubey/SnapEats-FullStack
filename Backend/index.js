3995c830d5a5a3cc61378879fbf9189e03a6039d
Kumar Ashish <princechaubey1895@gmail.com>
2025-07-04 10:42:53 +0530

    Added the Complete API end point for the Food

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const foodRouter=require("./routes/foodRoute");
 require("dotenv").config();
 
 const app = express();
@@ -9,11 +10,16 @@ const port = process.env.PORT || 8080;
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use("/uploads",express.static("uploads"));
 
 
 
 // Connect to database
 connectDB();
+
+
+//API's complete end points
+app.use("/api/food",foodRouter);
 
 
 // Test routes
