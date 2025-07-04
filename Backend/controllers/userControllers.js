26817dc7212fc9fbc22377c97b29c9c3b395c000
Kumar Ashish <princechaubey1895@gmail.com>
2025-07-04 10:47:04 +0530

    Implemented the UserModel and Controllers

---
+++ Backend/controllers/userControllers.js
@@ -0,0 +1,19 @@
+const userModel = require("../models/userModel");
+
+
+
+
+//Controller for the Login
+const login=async(req,res)=>{
+
+}
+
+
+//Controller for the register
+const register=async(req,res)=>{
+
+}
+
+
+
+module.exports={login,register};
